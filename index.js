9f3736ddc11165b0e7d5b85d4a54f1e07089204f
Renato Matos <renato.matos@specsavers.com>
2025-02-19 23:12:16 +0000

    fixing table already exists

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-import { TableClient, AzureNamedKeyCredential, odata } from "@azure/data-tables";
+import { TableClient, AzureNamedKeyCredential, TableServiceClient } from "@azure/data-tables";
 
 // Your Azure Storage Account details
 const accountName = process.env.AZURE_ACCOUNT_NAME ?? "";
 const accountKey = process.env.AZURE_ACCOUNT_KEY ?? "";
-const tableName = "CustomersTableV4";
+const tableName = "CustomersTable";
 
 console.log('Settings: ', {  accountName, accountKey, tableName });
 
 // Create a TableClient
 const credential = new AzureNamedKeyCredential(accountName, accountKey);
 const tableClient = new TableClient(`https://${accountName}.table.core.windows.net`, tableName, credential);
-
-async function tableExists() {
-    try {
-        await tableClient.getAccessPolicy();
-        return true;
-    } catch (error) {
-        console.log('tableExists status code: ', error.statusCode);
-        if (error.statusCode === 404) {
-        return false;
-        }
-        throw error;
+const serviceClient = new TableServiceClient(
+  `https://${accountName}.table.core.windows.net`,
+  credential
+);
+async function createTableIfNeeded() {
+  console.log(`Creating table  ${tableName} ...`);
+  await serviceClient.createTable(tableName, {
+    onResponse: (response) => {
+      console.log('createTableIfNeeded response: ', response.status);
+      if (response.status === 409) {
+        console.log(`Table ${tableName} already exists`);
+      }
     }
+  });
 }
 
 async function getEntity(partitionKey, rowKey) {
@@ -30,24 +32,17 @@ async function getEntity(partitionKey, rowKey) {
         console.log(`Entity with partitionKey "${partitionKey}" and rowKey "${rowKey}" exists.`);
         return entity;
     } catch (error) {
-        if (error.statusCode === 404) {
-        console.log(`Entity with partitionKey "${partitionKey}" and rowKey "${rowKey}" does not exist.`);
-        return null;
+        if (error?.statusCode === 404) {
+          console.log(`Entity with partitionKey "${partitionKey}" and rowKey "${rowKey}" does not exist.`);
+          return null;
         }
         throw error;
     }
 }
 
 async function main() {
-  // 1. Create a table if it does not exist
-  const tableExistsResult = await tableExists();
-  if (tableExistsResult) {
-    console.log(`Table "${tableName}" already exists.`);
-  } else {
-    console.log("Creating table...");
-    await tableClient.createTable();
-    console.log(`Table "${tableName}" created.`);
-  }
+  // 1. Create the table if it does not exist
+  await createTableIfNeeded()
 
   // 2. Insert an entity
   const entity = {
@@ -55,7 +50,8 @@ async function main() {
     rowKey: "321",
     name: "John Doe",
     email: "john.doe@example.com",
-    age: 30
+    age: 30,
+    ttl: 5, // Entity will expire in 20 seconds
   };
 
   // 3. Retrieve an entity
@@ -71,26 +67,22 @@ async function main() {
   
   }
 
-
-
-
   // 4. Update an entity (Merge mode)
   console.log("Updating entity...");
   await tableClient.updateEntity(
-    { partitionKey: entity.partitionKey, rowKey: entities.rowKey, age: 31 }, // Partial update
+    { partitionKey: entity.partitionKey, rowKey: entity.rowKey, age: 55 }, // Partial update
     "Merge"
   );
   console.log("Entity updated.");
 
-  // 5. Query entities (filtering by age)
-  console.log("Querying entities...");
-  const entities = tableClient.listEntities({
-    queryOptions: { filter: odata`age eq 31` }
-  });
+  // 5. Update an entity (Replace mode)
+  const updContent = await getEntity(entity.partitionKey, entity.rowKey);
+  
+  const timestamp = new Date(updContent.timestamp);
+  timestamp.setSeconds(timestamp.getSeconds() + updContent.ttl)
+  
+  console.log("updContent: ", updContent.timestamp, updContent.ttl, 'expires: ', timestamp);
 
-  for await (const entity of entities) {
-    console.log("Queried Entity:", entity);
-  }
 
   // 6. Delete an entity
   //console.log("Deleting entity...");
