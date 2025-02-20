d6d6388c32b58dab5f3352e796b4457e33236b8c
renato matos <renato.matos79@gmail.com>
2025-02-20 19:41:11 +0000

    Fixing code

--- lib.ts
+++ lib.ts
@@ -1,25 +1,25 @@
 /* eslint-disable no-console */
-import { RestError, TableClient, TableServiceClient } from '@azure/data-tables'
+import {RestError, TableClient, TableServiceClient} from '@azure/data-tables'
 
 export const createTable = (
-  serviceClient: TableServiceClient,
-  tableName: string,
-  maxRetries: number,
-  retryDelayInMs: number
+    serviceClient: TableServiceClient,
+    tableName: string,
+    maxRetries: number,
+    retryDelayInMs: number
 ) => {
-  return executeWithRetry(
-    () => {
-      return serviceClient.createTable(tableName, {
-        onResponse: (response) => {
-          if (response.status === 409) {
-            console.log(`Table ${tableName} already exists`);
-          }
-        }
-      })
-    },
-    maxRetries,
-    retryDelayInMs
-  )
+    return executeWithRetry(
+        () => {
+            return serviceClient.createTable(tableName, {
+                onResponse: (response) => {
+                    if (response.status === 409) {
+                        console.log(`Table ${tableName} already exists`);
+                    }
+                }
+            })
+        },
+        maxRetries,
+        retryDelayInMs
+    )
 }
 
 export const dropTable = (
@@ -27,118 +27,119 @@ export const dropTable = (
     tableName: string,
     maxRetries: number,
     retryDelayInMs: number
-  ) => {
-  return executeWithRetry(
-    () => {
-      return serviceClient.deleteTable(tableName, {
-        onResponse: (response) => {
-          console.log(`Table ${tableName} deleted with status ${response}`);
-          
-          if (response.status === 404) {
-            console.log(`Table ${tableName} not found`);
-          }
-        }
-      })
-    },
-    maxRetries,
-    retryDelayInMs
-  )
+) => {
+    return executeWithRetry(
+        () => {
+            return serviceClient.deleteTable(tableName, {
+                onResponse: (response) => {
+                    console.log(`Table ${tableName} deleted with status ${response}`);
+
+                    if (response.status === 404) {
+                        console.log(`Table ${tableName} not found`);
+                    }
+                }
+            })
+        },
+        maxRetries,
+        retryDelayInMs
+    )
 }
 
 export const executeWithRetry = async (
-  func: () => Promise<any>,
-  maxRetries: number,
-  retryDelayInMs: number,
-  noRetryOnResourceNotFound: boolean = false
+    func: () => Promise<any>,
+    maxRetries: number,
+    retryDelayInMs: number,
+    noRetryOnResourceNotFound: boolean = false
 ) => {
-  let numRetries = 0
-  let errorToReturn: any = null
-  while (numRetries < maxRetries) {
-    try {
-      return await func()
-    } catch (error) {
-      errorToReturn = error
-      if (noRetryOnResourceNotFound && isResourceNotFoundError(error)) {
-        break
-      }
-      console.error(
-        `Error executing function ${func.name}: ${
-          error instanceof RestError
-        } ${error.message}`
-      )
-      numRetries++
-      await new Promise(resolve => setTimeout(resolve, retryDelayInMs))
+    let numRetries = 0
+    let errorToReturn: any = null
+    while (numRetries < maxRetries) {
+        try {
+            return await func()
+        } catch (error) {
+            errorToReturn = error
+            if (noRetryOnResourceNotFound && isResourceNotFoundError(error)) {
+                break
+            }
+            console.error(
+                `Error executing function ${func.name}: ${
+                    error instanceof RestError
+                } ${error.message}`
+            )
+            numRetries++
+            await new Promise(resolve => setTimeout(resolve, retryDelayInMs))
+        }
     }
-  }
-  console.error(
-    `executeWithRetry: Function failed after ${numRetries + 1} retries.`
-  )
-  throw (
-    errorToReturn ??
-    new Error(`Function failed after ${numRetries + 1} retries.`)
-  )
+    console.error(
+        `executeWithRetry: Function failed after ${numRetries + 1} retries.`
+    )
+    throw (
+        errorToReturn ??
+        new Error(`Function failed after ${numRetries + 1} retries.`)
+    )
 }
 
 export const getEntity = (
-  tableClient: TableClient,
-  { partitionKey, rowKey }: { partitionKey: string, rowKey: string },
-  maxRetries: number,
-  retryDelayInMs: number
+    tableClient: TableClient,
+    {partitionKey, rowKey}: { partitionKey: string, rowKey: string },
+    maxRetries: number,
+    retryDelayInMs: number
 ) => {
-  return executeWithRetry(
-    () => tableClient.getEntity(partitionKey, rowKey),
-    maxRetries,
-    retryDelayInMs,
-    true
-  )
+    return executeWithRetry(
+        () => tableClient.getEntity(partitionKey, rowKey),
+        maxRetries,
+        retryDelayInMs,
+        true
+    )
 }
 
 export const createEntity = <T extends { partitionKey: string, rowKey: string }>(
-  tableClient: TableClient,
-  entity: T,
-  maxRetries: number,
-  retryDelayInMs: number
+    tableClient: TableClient,
+    entity: T,
+    maxRetries: number,
+    retryDelayInMs: number
 ) => {
-  return executeWithRetry(
-    () => tableClient.createEntity(entity),
-    maxRetries,
-    retryDelayInMs
-  )
+    return executeWithRetry(
+        () => tableClient.createEntity(entity),
+        maxRetries,
+        retryDelayInMs
+    )
 }
 
 
 export const deleteEntity = <T extends { partitionKey: string, rowKey: string }>(
-  tableClient: TableClient,
-  entity: T,
-  maxRetries: number,
-  retryDelayInMs: number
+    tableClient: TableClient,
+    entity: T,
+    maxRetries: number,
+    retryDelayInMs: number
 ) => {
-  const { partitionKey, rowKey } = entity
-  
-  return executeWithRetry(
-    () => tableClient.deleteEntity(partitionKey, rowKey),
-    maxRetries,
-    retryDelayInMs
-  )
+    const {partitionKey, rowKey} = entity
+
+    return executeWithRetry(
+        () => tableClient.deleteEntity(partitionKey, rowKey),
+        maxRetries,
+        retryDelayInMs,
+        true
+    )
 }
 
 export const updateEntity = <T extends { partitionKey: string, rowKey: string }>(
-  tableClient: TableClient,
-  entity: T,
-  maxRetries: number,
-  retryDelayInMs: number
+    tableClient: TableClient,
+    entity: T,
+    maxRetries: number,
+    retryDelayInMs: number
 ) => {
-  return executeWithRetry(
-    () => tableClient.updateEntity(entity, "Merge"),
-    maxRetries,
-    retryDelayInMs
-  )
+    return executeWithRetry(
+        () => tableClient.updateEntity(entity, "Merge"),
+        maxRetries,
+        retryDelayInMs
+    )
 }
 
 export const isStatusCodeError = (error: Error, statusCode: number) => {
-  return error instanceof RestError && error.statusCode === statusCode
+    return error instanceof RestError && error.statusCode === statusCode
 }
 
 export const isResourceNotFoundError = (error: Error) => {
-  return isStatusCodeError(error, 404)
+    return isStatusCodeError(error, 404)
 }
